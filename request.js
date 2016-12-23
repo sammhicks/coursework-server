df0e556dcd5ac4265590537b8fecda49ebda7ba8
Samuel Hicks <sh14820@my.bristol.ac.uk>
2016-12-23 14:30:25 +0000

    Requests now parse the url and expose the path and query

--- request.js
+++ request.js
@@ -1,9 +1,16 @@
 ﻿"use strict"
 
+const url = require('url');
+
 class Request {
   constructor(request, response) {
     this.request = request;
     this.response = response;
+
+    var requestURL = url.parse(request.url, true);
+
+    this.path = requestURL.path;
+    this.query = requestURL.query;
   }
 }
 
