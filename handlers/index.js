f099918e31beb1cd4ef2b4903ab9712b3d16f83a
Samuel Hicks <sh14820@my.bristol.ac.uk>
2016-12-26 16:46:41 +0000

    Created index for handlers folder

---
+++ handlers/index.js
@@ -0,0 +1,10 @@
+﻿"use strict";
+
+exports.Database = require("./database").DatabaseHandler;
+exports.Directory = require("./directory").DirectoryHandler;
+exports.Domain = require("./domain").DomainHandler;
+exports.Error = require("./error").ErrorHandler;
+exports.Handler = require("./handler").Handler;
+exports.HelloWorld = require("./hello-world").HelloWorldHandler;
+exports.Leaf = require("./leaf").LeafHandler;
+exports.RootDomain = require("./root-domain").RootDomainHandler;
