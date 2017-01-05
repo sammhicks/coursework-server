80268a6de32078c8783d6b4b6b93ad41a5119f01
Samuel Hicks <sh14820@my.bristol.ac.uk>
2017-01-05 23:58:24 +0000

    Created simple file handler

---
+++ handlers/file.js
@@ -0,0 +1,29 @@
+﻿"use strict";
+
+const fs = require("fs");
+const Handler = require("./handler").Handler;
+const HttpStatus = require("http-status-codes");
+const path = require("path");
+
+const mimeTypes = {
+  ".txt": "text/plain"
+};
+
+class File extends Handler {
+  constructor(serverPath) {
+    super();
+
+    this.serverPath = serverPath;
+  }
+
+  handleRequest(request) {
+    request.response.setHeader("Content-Type", mimeTypes[path.extname(this.serverPath)]);
+
+    request.response.writeHead(HttpStatus.OK);
+
+    request.response.end(fs.readFileSync(this.serverPath));
+  }
+}
+
+exports.File = File;
+exports.mimeTypes = mimeTypes;
--- handlers/index.js
+++ handlers/index.js
@@ -4,6 +4,7 @@ exports.Database = require("./database").DatabaseHandler;
 exports.Directory = require("./directory").DirectoryHandler;
 exports.Domain = require("./domain").DomainHandler;
 exports.Error = require("./error").ErrorHandler;
+exports.File = require("./file").File;
 exports.Handler = require("./handler").Handler;
 exports.HelloWorld = require("./hello-world").HelloWorldHandler;
 exports.Leaf = require("./leaf").LeafHandler;
