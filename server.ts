f0eaa73e77400df180a3a3ccf46b0345389aa92d
Samuel Hicks <sh14820@my.bristol.ac.uk>
2017-02-07 17:23:36 +0000

    Moved server.ts back to the root directory

--- server.ts
+++ server.ts
@@ -1,18 +1,16 @@
 ﻿"use strict";
 
-import { handlers } from "./html-handlers";
-import { Error, ErrorHandler, Handler, HTMLHandler, LeafHandler } from "../handlers";
+import { handlers } from "./server/html-handlers";
+import { Error, ErrorHandler, Handler, HTMLHandler, LeafHandler } from "./handlers";
 import { IncomingMessage, ServerResponse, createServer } from "http";
 import * as path from "path";
 
-import { Crawler } from "../crawler";
-import { Request } from "../request";
+import { Crawler } from "./crawler";
+import { Request } from "./request";
 
 const port: number = process.env.PORT || 8080;
 
-const serverPath = path.resolve(__dirname, "data");
-
-const crawledHandler = new Crawler().crawl(serverPath, { namedHandlers: { document: handlers.document } });
+const crawledHandler = new Crawler().crawl("server/data", { namedHandlers: { document: handlers.document } });
 
 const errorHandler = new ErrorHandler(crawledHandler, (error: Error) => Handler.handleError(error.request, error.errorCode));
 
