4246324d3259524df4b53ddc03d19bd344d1f82c
Samuel Hicks <sh14820@my.bristol.ac.uk>
2017-05-24 16:56:48 +0100

    Created throttle

---
+++ promises/throttle.ts
@@ -0,0 +1,44 @@
+export class Throttle {
+    private _timeoutInstance: NodeJS.Timer | null;
+    private _queue: (() => void)[];
+
+    constructor(private timeout: number) {
+        this._timeoutInstance = null;
+        this._queue = [];
+    }
+
+    access<PassType>() {
+        const self = this;
+
+        function setThrottleTimeout() {
+            self._timeoutInstance = setTimeout(handleThrottleTimeout, self.timeout);
+        }
+
+        function handleThrottleTimeout() {
+            const front = self._queue.shift();
+
+            if (self._queue.length == 0) {
+                self._timeoutInstance = null;
+            } else {
+                setThrottleTimeout();
+            }
+
+            if (front != undefined) {
+                front();
+            }
+        }
+
+        return function throttleQueue(passValue: PassType) {
+            return new Promise<PassType>(function executor(resolve, reject) {
+                if (self._timeoutInstance == null) {
+                    resolve(passValue);
+                    setThrottleTimeout();
+                } else {
+                    self._queue.push(function handleThrottleQueueItem() {
+                        resolve(passValue);
+                    });
+                }
+            });
+        }
+    }
+}
