b5b93004d495199ee449dd2aaee9e822de2b788e
Samuel Hicks <sh14820@my.bristol.ac.uk>
2017-05-26 16:54:41 +0100

    Reincluded tag table creation

--- server/database/gen.sql
+++ server/database/gen.sql
@@ -81,6 +81,24 @@ CREATE TABLE "videos" (
 
 -- Video Tags
 
+CREATE TABLE "country_tags" (
+    "video_id"      TEXT,
+    "country_id"    INTEGER,
+    FOREIGN KEY ("video_id") REFERENCES "videos"("id"),
+    FOREIGN KEY ("country_id") REFERENCES "countries"("id")
+);
+
+CREATE INDEX "country_tag_videos" ON "country_tags" ("video_id");
+
+CREATE TABLE "competition_tags" (
+    "video_id"          TEXT,
+    "competition_id"    INTEGER,
+    FOREIGN KEY ("video_id") REFERENCES "videos"("id"),
+    FOREIGN KEY ("competition_id") REFERENCES "competitions"("id")
+);
+
+CREATE INDEX "competition_tag_videos" ON "competition_tags" ("video_id");
+
 CREATE TABLE "team_tags" (
     "video_id"      TEXT,
     "team_id"       INTEGER,
