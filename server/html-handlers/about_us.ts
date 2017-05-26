74e8bb6dc4ae61bb61d41fc52090400da7c8eb05
Unknown <spiritbeast26@yahoo.co.uk>
2017-05-26 16:59:27 +0100

    about page done, png done

--- server/html-handlers/about_us.ts
+++ server/html-handlers/about_us.ts
@@ -4,20 +4,22 @@ import { Page } from "./page";
 
 var imageWrap = new Element("div", { class: "image-wrap" }, [
     new Element("img", { src: "images/about.png", alt: "Team Photo" }, []),
+    new Element("div", { class: "bio-circle", id: "bio-circle-sam" }, []),
     new Element("div", { class: "bios", id: "samsBio" }, [
         new Element("span", {}, [
             new String("Samuel Hicks")
         ]),
         new Element("span", {}, [
-            new String("Programming wizard. Old man. Lead back-end developer. Not sure if he is not actually a lizard person.")
+            new String("Programming wizard. Old man. Lead back-end developer. May actually be a lizard person. Handy with an accordian.")
         ]),
     ]),
+    new Element("div", { class: "bio-circle", id: "bio-circle-mike" }, []),
     new Element("div", { class: "bios", id: "mikesBio" }, [
         new Element("span", {}, [
             new String("Michael Nicholas")
         ]),
         new Element("span", {}, [
-            new String("Basically Picasso. Young Upstart. Lead fron-end developer. ")
+            new String("Basically Picasso. Young Upstart. Lead front-end developer. His mixtape is pure fire. Top footballer too I've heard.")
         ]),
     ])
 ]);
